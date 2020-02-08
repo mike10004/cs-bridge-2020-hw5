8dabcdf67449bb17b92169f512ea7eb4e91c936e
Mike Chaberski <mchaberski@gmail.com>
2020-02-07 22:30:07 -0500

    q2: support getting num from environment

--- q2/main.cpp
+++ q2/main.cpp
@@ -1,4 +1,4 @@
-#pragma clang diagnostic push                       // stage:remove
+#pragma clang diagnostic push                       // stage:remove                       
 #pragma ide diagnostic ignored "hicpp-use-nullptr"  // stage:remove
 #pragma ide diagnostic ignored "cert-msc30-c"       // stage:remove
 #pragma ide diagnostic ignored "cert-msc32-c"       // stage:remove
@@ -8,20 +8,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <climits>  // stage:remove
 
 using namespace std;
 
 const int MAX_GUESS_COUNT = 5;
 const int RANDOM_MIN = 1;
 const int RANDOM_MAX = 100;
+const char* ENV_NUM = "Q2_RANDOM_NUMBER";   // stage:remove
 
 int main() {
     int userGuess;
     int userGuessLowerBound = RANDOM_MIN, userGuessUpperBound = RANDOM_MAX;
     int numGuesses = 0;
     int randomNumber;
+    
     srand(time(0));
     randomNumber = (rand() % (RANDOM_MAX - RANDOM_MIN + 1)) + 1;
+                                                                                                        // stage:remove
+    const char* envNumStr = getenv(ENV_NUM);                                                           // stage:remove
+    if (envNumStr != nullptr) {                                                                         // stage:remove
+        long envNum = strtol(envNumStr, nullptr, 10);                                       // stage:remove
+        if (envNum > 0 && envNum <= INT_MAX) {                                                          // stage:remove
+            randomNumber = (int) envNum;                                                                // stage:remove
+        } else {                                                                                        // stage:remove
+            cerr << ENV_NUM << "=" << envNumStr << " but that is not a valid int for this program\n";   // stage:remove
+        }                                                                                               // stage:remove
+    }                                                                                                   // stage:remove
     
     cout << "I thought of a number between " << RANDOM_MIN << " and " << RANDOM_MAX << "! "; 
     cout << "Try to guess it.\n";
@@ -57,4 +70,4 @@ int main() {
     return 0;
 }
 
-#pragma clang diagnostic pop
+#pragma clang diagnostic pop   // stage:remove
