b2a3d4cc11008cec717b8573888a23d6a82aa1e6
Mike Chaberski <mchaberski@gmail.com>
2020-02-07 21:35:32 -0500

    q1 complete

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,11 +1,24 @@
-// Question 1
+// mac937@nyu.edu
+// HW5 Question 1
 
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    cout << "q1 executed" << endl;
+const char TAB = '\t';
+
+int main() {
+    int inputNumber;
+    cout << "Please enter a positive integer:\n";
+    cin >> inputNumber;
+    for (int row = 1; row <= inputNumber; row++) {
+        for (int col = 1; col <= inputNumber; col++) {
+            if (col > 1) {
+                cout << TAB;
+            }
+            cout << row * col;
+        }
+        cout << endl;
+    }
     return 0;
 }
